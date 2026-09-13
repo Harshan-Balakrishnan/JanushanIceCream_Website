b85096b9cac13681644716a9341eef13a9b9f31e
Harshan Balakrishnan <harisharshan2004@gmail.com>
2026-09-13 19:22:09 +0530

    Load cohesive JIC theme stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { SITE_DESCRIPTION, SITE_NAME, SITE_URL } from "@/lib/site";
 import "./globals.css";
 import "./jic-upgrades.css";
 import "./flavour-mobile.css";
+import "./jic-theme.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
