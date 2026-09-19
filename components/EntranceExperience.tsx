2bf9389abd7f1c9ab7f02e7cd3ce612939cdad09
Harshan Balakrishnan <harisharshan2004@gmail.com>
2026-09-19 23:47:13 +0530

    fix: place hero social icons with CTA content

--- components/EntranceExperience.tsx
+++ components/EntranceExperience.tsx
@@ -21,17 +21,7 @@ export default function EntranceExperience() {
       <div className="ambient ambient-two" />
       <div className="cinema-vignette" aria-hidden="true" />
       <div className="stars" aria-hidden="true" />
-      <div className="hero-socials" aria-label="Janushan Ice Cream social links">
-        <a href="https://www.instagram.com/janushan_ice_cream/" target="_blank" rel="noreferrer" aria-label="Instagram">
-          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7.8 2h8.4A5.8 5.8 0 0 1 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8A5.8 5.8 0 0 1 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2Zm0 1.8A4 4 0 0 0 3.8 7.8v8.4a4 4 0 0 0 4 4h8.4a4 4 0 0 0 4-4V7.8a4 4 0 0 0-4-4H7.8Zm8.85 1.35a1.08 1.08 0 1 1 0 2.16 1.08 1.08 0 0 1 0-2.16ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.8a3.2 3.2 0 1 0 0 6.4 3.2 3.2 0 0 0 0-6.4Z"/></svg>
-        </a>
-        <a href="https://www.facebook.com/janushanicecream" target="_blank" rel="noreferrer" aria-label="Facebook">
-          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07c0 6.02 4.39 11 10.13 11.88v-8.4H7.08v-3.48h3.05V9.43c0-3.02 1.79-4.69 4.53-4.69 1.31 0 2.69.24 2.69.24v2.97h-1.52c-1.49 0-1.95.93-1.95 1.88v2.24h3.32l-.53 3.48h-2.79v8.4C19.61 23.07 24 18.09 24 12.07Z"/></svg>
-        </a>
-        <a href="https://www.google.com/search?q=Janushan+Ice+Cream+Vavuniya" target="_blank" rel="noreferrer" aria-label="Google reviews">
-          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M21.35 12.27c0-.73-.07-1.44-.2-2.12H12v4.02h5.23a4.47 4.47 0 0 1-1.94 2.94v2.45h3.14c1.84-1.69 2.92-4.18 2.92-7.29ZM12 21c2.63 0 4.84-.87 6.43-2.36l-3.14-2.45c-.87.58-1.98.93-3.29.93-2.53 0-4.68-1.71-5.45-4.01H3.3v2.53A9.71 9.71 0 0 0 12 21Zm-8.7-5.36 3.25-2.53a5.83 5.83 0 0 1 0-3.72L3.3 6.86A9.98 9.98 0 0 0 2 12c0 1.65.4 3.21 1.3 4.64ZM12 5.85c1.43 0 2.71.49 3.72 1.45l2.79-2.79C16.83 2.92 14.62 2 12 2a9.71 9.71 0 0 0-8.7 4.86l3.25 2.53C7.32 7.56 9.47 5.85 12 5.85Z"/></svg>
-        </a>
-      </div>
+
 
       <motion.div
         className="brand-aura"
@@ -251,6 +241,18 @@ export default function EntranceExperience() {
           <a className="button button-ghost" href="https://wa.me/94776015041?text=Hi%20Janushan%20Ice%20Cream!%20I'd%20like%20to%20ask%20about%20your%20menu." target="_blank" rel="noreferrer">Order on WhatsApp ↗</a>
           <a className="hero-text-link" href="#flavours">Explore Flavours ↓</a>
         </div>
+
+      <div className="hero-socials" aria-label="Janushan Ice Cream social links">
+        <a href="https://www.instagram.com/janushan_ice_cream/" target="_blank" rel="noreferrer" aria-label="Instagram">
+          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7.8 2h8.4A5.8 5.8 0 0 1 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8A5.8 5.8 0 0 1 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2Zm0 1.8A4 4 0 0 0 3.8 7.8v8.4a4 4 0 0 0 4 4h8.4a4 4 0 0 0 4-4V7.8a4 4 0 0 0-4-4H7.8Zm8.85 1.35a1.08 1.08 0 1 1 0 2.16 1.08 1.08 0 0 1 0-2.16ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.8a3.2 3.2 0 1 0 0 6.4 3.2 3.2 0 0 0 0-6.4Z"/></svg>
+        </a>
+        <a href="https://www.facebook.com/janushanicecream" target="_blank" rel="noreferrer" aria-label="Facebook">
+          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07c0 6.02 4.39 11 10.13 11.88v-8.4H7.08v-3.48h3.05V9.43c0-3.02 1.79-4.69 4.53-4.69 1.31 0 2.69.24 2.69.24v2.97h-1.52c-1.49 0-1.95.93-1.95 1.88v2.24h3.32l-.53 3.48h-2.79v8.4C19.61 23.07 24 18.09 24 12.07Z"/></svg>
+        </a>
+        <a href="https://www.google.com/search?q=Janushan+Ice+Cream+Vavuniya" target="_blank" rel="noreferrer" aria-label="Google reviews">
+          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M21.35 12.27c0-.73-.07-1.44-.2-2.12H12v4.02h5.23a4.47 4.47 0 0 1-1.94 2.94v2.45h3.14c1.84-1.69 2.92-4.18 2.92-7.29ZM12 21c2.63 0 4.84-.87 6.43-2.36l-3.14-2.45c-.87.58-1.98.93-3.29.93-2.53 0-4.68-1.71-5.45-4.01H3.3v2.53A9.71 9.71 0 0 0 12 21Zm-8.7-5.36 3.25-2.53a5.83 5.83 0 0 1 0-3.72L3.3 6.86A9.98 9.98 0 0 0 2 12c0 1.65.4 3.21 1.3 4.64ZM12 5.85c1.43 0 2.71.49 3.72 1.45l2.79-2.79C16.83 2.92 14.62 2 12 2a9.71 9.71 0 0 0-8.7 4.86l3.25 2.53C7.32 7.56 9.47 5.85 12 5.85Z"/></svg>
+        </a>
+      </div>
       </motion.div>
 
       <motion.a
