60a2f916bc7200282099d21f9e7ba6e4ce02f6b2
Harshan Balakrishnan <harisharshan2004@gmail.com>
2026-09-22 14:51:19 +0530

    feat: add reusable mascot peek component

---
+++ components/MascotPeek.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+
+type MascotPeekProps = {
+  className?: string;
+  alt?: string;
+  priority?: boolean;
+};
+
+export default function MascotPeek({
+  className = "",
+  alt = "",
+  priority = false,
+}: MascotPeekProps) {
+  return (
+    <span className={"mascot-peek " + className} aria-hidden={alt ? undefined : true}>
+      <Image
+        src="/brand/mascot-peek.png"
+        alt={alt}
+        width={420}
+        height={350}
+        className="mascot-peek-image"
+        priority={priority}
+      />
+    </span>
+  );
+}
