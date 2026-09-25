8d8394fe97c4f270d5a9c694f178f50a25635f66
Harshan Balakrishnan <harisharshan2004@gmail.com>
2026-09-25 15:35:35 +0530

    Remove unwanted store status banner from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import EntranceExperience from "@/components/EntranceExperience";
 import DiscoverPreview from "@/components/DiscoverPreview";
 import CinematicFooter from "@/components/CinematicFooter";
 import StructuredData from "@/components/StructuredData";
-import StoreStatusBar from "@/components/StoreStatusBar";
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <StructuredData />
       <NavBar />
       <EntranceExperience />
-      <StoreStatusBar />
       <DiscoverPreview />
       <CinematicFooter />
     </main>
