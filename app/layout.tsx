b2a39b80707733ddd08a655289d59a9d8b079a2d
Harshan Balakrishnan <harisharshan2004@gmail.com>
2026-09-25 15:14:44 +0530

    Load heart-shaped story finale styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import OptionalAnalytics from "@/components/OptionalAnalytics";
 import { SITE_DESCRIPTION, SITE_NAME, SITE_URL } from "@/lib/site";
 import "./globals.css";
+import "./story-finale.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
